b473a4188473da4c80312a4264baf53af0a7c537
Million <million.mburu@student.moringaschool.com>
2026-06-06 19:29:50 +0300

    supabase

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "YOUR_PROJECT_URL";
-const supabaseKey = "YOUR_ANON_KEY";
+const supabaseUrl = "https://rtbheewnbtyoupcuwnpa.supabase.co/rest/v1/";
+const supabaseKey = "sb_publishable_1KFQvzNSGdmwUiZE1fS0rg_HqWKp1JN";
 
 export const supabase = createClient(
   supabaseUrl,
