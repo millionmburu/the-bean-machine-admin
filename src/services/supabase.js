8e30b525e4da4638d4d41f42e552cadf0fe01668
Million <million.mburu@student.moringaschool.com>
2026-06-06 19:35:16 +0300

    bug fix

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://rtbheewnbtyoupcuwnpa.supabase.co";
-const supabaseKey = "sb_publishable_1KFQvzNSGdmwUiZE1fS0rg_HqWKp1JN";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ0YmhlZXduYnR5b3VwY3V3bnBhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA3NTE2MDYsImV4cCI6MjA5NjMyNzYwNn0.wzxlJm37kCTc9FvMcRwIN7NnZPqtU72TYxFL6NargUA";
 
 export const supabase = createClient(
   supabaseUrl,
