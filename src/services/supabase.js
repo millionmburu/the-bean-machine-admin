d305c803ca1c1327fae04048a0ae9b322a83b990
Million <million.mburu@student.moringaschool.com>
2026-06-06 19:32:25 +0300

    bug fix

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://rtbheewnbtyoupcuwnpa.supabase.co/rest/v1/";
+const supabaseUrl = "https://rtbheewnbtyoupcuwnpa.supabase.co";
 const supabaseKey = "sb_publishable_1KFQvzNSGdmwUiZE1fS0rg_HqWKp1JN";
 
 export const supabase = createClient(
